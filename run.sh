68b847ba677c86f9001686131bbe06a23064ec9f
Matthias Pfeil <matthias.pfeil@wwu.de>
2017-06-23 15:41:07 +0200

    reset ORIGAMI_API_URL

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ IFS=$'\n\t'
 
 api_adr=${API_URL:-https://giv-origami.uni-muenster.de/origamidb}
 
-sed -i -e "s|ORIGAMI_API_URL|$api_adr|g" /usr/src/app/dist/js/services.js
+sed -i -e "s|ORIGAMI_API_URL|$api_adr|g" /usr/src/app/dist/js/services.js /usr/src/app/dist/js/services/authentication.js
 
 exec /bin/true
